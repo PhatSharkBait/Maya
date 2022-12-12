92fa929200bbb98b97458622a868a6fab9a512b2
Cameron Bass <Cameronjudd98@gmail.com>
2022-12-12 00:27:22 -0700

    color picker and select shapes working

--- Scripts/Python/colorOverride.py
+++ Scripts/Python/colorOverride.py
@@ -2,10 +2,23 @@ import maya.cmds as cmds
 
 
 def main():
-    cmds.pickWalk(d='down')
-    sels = cmds.ls(sl=True)
-    for obj in sels:
-        cmds.setAttr(obj)
+    cmds.colorEditor()
+    if cmds.colorEditor(query=True, result=True):
+        values = cmds.colorEditor(query=True, rgb=True)
+        change_colors(values)
+    else:
+        cmds.warning("color editor was dismissed")
+
+
+def change_colors(values):
+    sels = cmds.ls(selection=True)
+    for selection in sels:
+        selectionShape = cmds.listRelatives(selection, s=True)
+        cmds.setAttr(selectionShape[0] + ".overrideEnabled", True)
+        cmds.setAttr(selectionShape[0] + ".overrideRGBColors", True)
+        cmds.setAttr(selectionShape[0] + ".overrideColorR", values[0])
+        cmds.setAttr(selectionShape[0] + ".overrideColorG", values[1])
+        cmds.setAttr(selectionShape[0] + ".overrideColorB", values[2])
 
 
 if __name__ == '__main__':
